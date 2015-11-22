b93ca4c226c4c99d413ddda714eb56c2f3475244
Eric Ashmore <edashmore@gmail.com>
2015-11-21 17:41:23 -0800

    feat(bet): dynamic bet button disabled when bet is invalid

--- frontend/app/controllers/items/betting.js
+++ frontend/app/controllers/items/betting.js
@@ -2,23 +2,50 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Controller.extend({
+  totalValue: 0,
+
+  numberOfItems:0,
+
   actions: {
-    betItem(element, isBet) {
-      var elementId = element.get('id');
-      var htmlId = "#item" + elementId;
+    betItem(item, isBet) {
+      function isValidBet() {
+        if(that.totalValue >= 10 &&
+          (that.numberOfItems > 0 && that.numberOfItems < 11)
+        ){
+          $bettingButton.attr('disabled', false);
+        } else {
+          $bettingButton.attr('disabled', true);
+        }
+      }
+      var that = this;
+
+      var itemId = item.get('id');
+      var price = item.get('price');
+
+      var htmlId = "#item" + itemId;
       var $el = $(htmlId);
 
-      $el.remove();
+      var $bettingButton = $('.place-bet');
 
-      $el.attr("data-item-id", elementId);
+      $el.remove();
+      $el.attr("data-item-id", itemId);
 
       if (isBet) {
         $('.stash').append($el);
         isBet = false;
+        this.numberOfItems -= 1;
+        this.totalValue -= price;
       } else {
         $('.newbet').append($el);
         isBet = true;
+        this.numberOfItems += 1;
+        this.totalValue += price;
       }
+
+      Ember.Logger.log(this.totalValue);
+      Ember.Logger.log($bettingButton.attr('disabled'));
+
+      isValidBet();
     }
   }
 });
