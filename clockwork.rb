2db5f8ea0b8a31e02b8b62ecd182faf6d3950f9f
Eric Ashmore <edashmore@gmail.com>
2015-11-25 14:01:13 -0800

    fix: typo in clockwork

--- clockwork.rb
+++ clockwork.rb
@@ -10,12 +10,12 @@ module Clockwork
     puts "Running #{job}"
   end
 
-  every(1.day, 'start_match', :at => '20:00', :tz => 'PST') do
+  every(1.day, 'start_match', :at => '04:00', :tz => 'UTC') do
     match = Match.all.last
     match.simulate_match
   end
 
-  every(1.day, 'fill_db', :at => '00:00', :tz => 'PST') do
+  every(1.day, 'fill_db', :at => '08:00', :tz => 'UTC') do
     new_match = Match.create(
       { has_started: false, map: 'de_dust2', current_round: 0 }
     )
