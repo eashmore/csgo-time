0f9ce39381eabd8a0a2c0d236727521fbb8ab7a0
Eric Ashmore <edashmore@gmail.com>
2016-02-27 16:50:56 -0800

    refactor(match): move getPrizePool and getRecentBets to controller

--- frontend/app/controllers/matches/current.js
+++ frontend/app/controllers/matches/current.js
@@ -5,6 +5,75 @@ export default Ember.Controller.extend({
 
   timeLeft: '',
 
+  prizePool: 0,
+
+  getPrizePool(bets) {
+    var pool = 0;
+    bets.forEach(function(bet) {
+      pool += bet.get('totalValue');
+    });
+
+    this.set('prizePool', Math.round(pool));
+  },
+
+  getRecentBets(bets) {
+    var recentBets = bets.slice(0, 6);
+    for(var i = 0; i < recentBets.length; i++) {
+      this.store.findRecord('user', recentBets[i].get('userId'));
+    }
+
+    this.set('recentBets', recentBets);
+  },
+
+  //timer setup
+  timeUntilMatch(match) {
+    function updateTime() {
+      var startTime = match.get('startTime');
+
+      var timeLeft = new Date(startTime) - (new Date());
+      timeLeft = timeLeft / 1000;
+
+      var timeLeftString = timeLeft < 0 ? secToHours(0) : secToHours(timeLeft);
+      that.set('timeLeft', timeLeftString);
+
+      return timeLeft;
+    }
+
+    function secToHours(sec) {
+      var sec_num = parseInt(sec, 10);
+      var hours = Math.floor(sec_num / 3600);
+      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      var time = hours + ' hours : ' + minutes + ' mins : ' + seconds + ' secs';
+      return time;
+    }
+
+    var that = this;
+    updateTime();
+    var timer = setInterval(function() {
+      var timeLeft = updateTime();
+      if (timeLeft <= 0) {
+        match.reload();
+      }
+
+      if (match.get('hasStarted')) {
+        clearInterval(timer);
+        that.transitionToRoute('matches.inprogress');
+      }
+    }, 1000);
+  },
+
   // payout and rake
   payBets(match) {
     function getPayoutRatio(bets) {
--- frontend/app/routes/matches/current.js
+++ frontend/app/routes/matches/current.js
@@ -10,12 +10,11 @@ export default Ember.Route.extend({
 
     var bets = match.get('bets');
     if (bets.get('length')) {
-      this.prizePool(match, bets);
-      this.getRecentBets(bets, controller);
+      controller.getPrizePool(bets);
+      controller.getRecentBets(bets);
     }
 
-    this.timeUntilMatch(match, controller);
-
+    controller.timeUntilMatch(match);
     if (match.get('hasStarted')) {
       this.transitionTo('matches.inprogress');
     }
@@ -37,73 +36,5 @@ export default Ember.Route.extend({
     });
 
     this.store.findRecord('team', model.get('teamId'));
-  },
-
-  prizePool(match, bets) {
-    var pool = 0;
-    bets.forEach(function(bet) {
-      pool += bet.get('totalValue');
-    });
-
-    match.set('prizePool', Math.round(pool));
-    match.save();
-  },
-
-  getRecentBets(bets, controller) {
-    var recentBets = bets.slice(0, 6);
-    for(var i = 0; i < recentBets.length; i++) {
-      this.store.findRecord('user', recentBets[i].get('userId'));
-    }
-
-    controller.set('recentBets', recentBets);
-  },
-
-  //timer setup
-  timeUntilMatch(match, controller) {
-    function updateTime() {
-      var startTime = match.get('startTime');
-
-      var timeLeft = new Date(startTime) - (new Date());
-      timeLeft = timeLeft / 1000;
-
-      var timeLeftString = timeLeft < 0 ? secToHours(0) : secToHours(timeLeft);
-      controller.set('timeLeft', timeLeftString);
-
-      return timeLeft;
-    }
-
-    function secToHours(sec) {
-      var sec_num = parseInt(sec, 10);
-      var hours = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-      if (hours < 10) {
-        hours = "0" + hours;
-      }
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-
-      var time = hours + ' hours : ' + minutes + ' mins : ' + seconds + ' secs';
-      return time;
-    }
-
-    var that = this;
-    updateTime();
-    var timer = setInterval(function() {
-      var timeLeft = updateTime();
-      if (timeLeft <= 0) {
-        match.reload();
-      }
-
-      if (match.get('hasStarted')) {
-        clearInterval(timer);
-        that.transitionToRoute('matches.inprogress');
-      }
-    }, 1000);
   },
 });
